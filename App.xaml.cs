ec2d7cd75de6ccaf8befeebd5119a74b49ee487a
kazumori102 <kazukazu323rx@gmail.com>
2024-01-21 02:13:35 +0900

    Readmeの通りD&Dに対応させた。

--- App.xaml.cs
+++ App.xaml.cs
@@ -11,5 +11,8 @@ namespace PrompReader {
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application {
+		private void Application_Startup(object sender, StartupEventArgs e) {
+			Arg.argv = e.Args;
+		}
 	}
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,7 +19,16 @@ namespace PrompReader {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+
+	public static partial class Arg {
+		public static string[] argv;
+	}
+
 	public partial class MainWindow : Window {
+
+		
+
+
 		public MainWindow() {
 			InitializeComponent();
 
@@ -30,27 +39,34 @@ namespace PrompReader {
 				args.Handled = true;
 			};
 
+			if (Arg.argv.Length != 0) {
+				string? parameters = ReadPngParameters(Arg.argv);
+				if (parameters != null) {
+					TextBoxParameters.Text = parameters;
+				}
+			}
 
 			TextBoxParameters.PreviewDrop += (sender, args) => {
 				if (!args.Data.GetDataPresent(DataFormats.FileDrop)) { return; }
 
 				string[] fileNames = (string[])args.Data.GetData(DataFormats.FileDrop);
-				if ((fileNames == null) || (fileNames.Length < 0)) { return; }
-				string filePath = fileNames[0];
-				string parameters = ReadPngParameters(filePath);
+
+				string? parameters = ReadPngParameters(fileNames);
 				if (parameters != null) {
 					TextBoxParameters.Text = parameters;
 				}
 			};
 		}
 		
-		private string ReadPngParameters(string filePath) {
+		private string? ReadPngParameters(string[] ListFilePath) {
 			/*
 			 https://www.setsuki.com/hsp/ext/png.htm
 			 https://hoshi-sano.hatenablog.com/entry/2013/08/18/112550
 			 https://ja.wikipedia.org/wiki/Portable_Network_Graphics
 			 https://edom18.hateblo.jp/entry/2022/02/14/082055
 			 */
+			if ((ListFilePath == null) || (ListFilePath.Length < 0)) { return null; }
+			string filePath = ListFilePath[0];
 			string key = "parameters";
 			using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read)) {
 				byte[] buffer = new byte[8];
@@ -81,7 +97,7 @@ namespace PrompReader {
 					else if (chunkType == "iTXt") {
 						buffer = new byte[chunkLength];
 						fs.Read(buffer, 0, chunkLength);
-						string res = ParseiTXtChunk(buffer, key);
+						string? res = ParseiTXtChunk(buffer, key);
 						if (res != null) { return res; }
                     }
 
@@ -96,7 +112,7 @@ namespace PrompReader {
 		}
 
 
-		static string ParseiTXtChunk(byte[] data, string key) {
+		static string? ParseiTXtChunk(byte[] data, string key) {
 			int index = 0;
 			string keyword = GetString(data, ref index);
 			if(keyword != key) { return null; }
@@ -120,6 +136,7 @@ namespace PrompReader {
 			while ((data[index++] != 0)&& (index<data.Length)) ;
 			return Encoding.UTF8.GetString(data, start, index - start - 1);
 		}
-
 	}
+
+
 }
